7d9206513fc224579388d484a77b4c6a1a07c189
ModifiedDragon <nick-schroeder@gmx.net>
2025-09-10 09:25:39 +0200

    calc_tasks

---
+++ src/main/java/nscr/Calc_Task.java
@@ -0,0 +1,73 @@
+package nscr;
+
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+public class Calc_Task {
+    double zahl1 = 142.7;
+    double zahl2 = 134.5;
+    double ergebnis;
+    String operator = "+";
+    Scanner scanner = new Scanner(System.in);
+
+    public void afg() {
+        System.out.println("Gebe deine erste Zahl an: ");
+        zahl1 = scanner.nextInt();
+        System.out.println("Bitte gebe den Operator an: ");
+        operator = scanner.next().toLowerCase();
+        if (operator.equals("wurzel")) {
+            ergebnis = root(zahl1);
+
+        } else {
+            System.out.println("Gebe deine zweite Zahl an: ");
+            zahl2 = scanner.nextInt();
+            double ergebisa[] = new double[5];
+            ergebisa[0] = berechneErgebnis(zahl1, zahl2, "+");
+            ergebisa[1] = berechneErgebnis(zahl1, zahl2, "-");
+            ergebisa[2] = berechneErgebnis(zahl1, zahl2, "*");
+            ergebisa[3] = berechneErgebnis(zahl1, zahl2, "/");
+            ergebisa[4] = berechneErgebnis(zahl1, zahl2, "^");
+            System.out.println(ergebisa[0]);
+            System.out.println(ergebisa[1]);
+            System.out.println(ergebisa[2]);
+            System.out.println(ergebisa[3]);
+            System.out.println(ergebisa[4]);
+            ergebnis = berechneErgebnis(zahl1, zahl2, operator);
+            if (ergebnis % (int) ergebnis == 0) {
+                System.out.println((int) ergebnis);
+            }
+            System.out.println(ergebnis);
+            weiter();
+        }
+    }
+
+    private void weiter() {
+        System.out.println("Willst du weitere Berechnungen machen? (y/n)");
+        String a = scanner.next();
+        if (a.equals("y")) {
+            afg();
+        } else if (a.equals("n")) {
+            exit(1);
+        } else {
+            System.out.println("Bitte gebe ein 'y' oder 'n' an.");
+            weiter();
+        }
+    }
+
+    private double berechneErgebnis(double z1, double z2, String op) {
+        double ergebnis = 0;
+        switch (op){
+            case "+" -> ergebnis = z1 + z2;
+            case "-" -> ergebnis = z1 - z2;
+            case "*" -> ergebnis = z1 * z2;
+            case "/" -> ergebnis = z1 / z2;
+            case "^" -> ergebnis = Math.pow(z1, z2);
+        }
+        return ergebnis;
+    }
+
+    public double root(double z) {
+        return Math.sqrt(z);
+    }
+}
--- src/main/java/nscr/main.java
+++ src/main/java/nscr/main.java
@@ -2,7 +2,9 @@ package nscr;
 
 public class main {
     public static void main(String[] args){
-        Calculator calc = new Calculator();
-        calc.startup();
+        //Calculator calc = new Calculator();
+        //calc.startup();
+        Calc_Task calcTask = new Calc_Task();
+        calcTask.afg();
     }
 }
